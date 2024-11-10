4aa3d51a079408558420799689827f5e33f9b71e
poultergeist <vlogatskiy3@gmail.com>
2024-11-11 01:38:59 +0200

    Updated files

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,69 @@
-import 'dart:math';
+import 'package:flutter/material.dart';
 
 void main() {
-  const valueFrom = -3.0;
-  const valueTo = 8.0;
-  const step = 0.5;
-
-  print("y = x^2 - 2 * x^3\n");
-
-  var x = valueFrom + step;
-  var sum = 0.0;
-  var count = 0;
-
-  for (x; x < valueTo; x+=step) {
-    final y = calc(x);
-    print("x = $x, $x^2 - 2 * $x^3 = $y");
-    sum += y;
-    count++;
-  }
-
-  final mean = sum / count;
-  print("\nmean = $mean");
+  runApp(const MyApp());
 }
 
-double calc(double x) {
-  return x * x - 2 * x * x * x;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'IPZ-33: Vladyslav\'s First Flutter App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter += 4;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.pets),
+      ),
+    );
+  }
 }
