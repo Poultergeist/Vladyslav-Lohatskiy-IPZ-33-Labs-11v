bdffa527a1f317a17202acdb16d22def083eabd8
poultergeist <vlogatskiy3@gmail.com>
2024-11-11 01:54:23 +0200

    Updated files for lab 5

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,24 @@ void main() {
   runApp(const MyApp());
 }
 
+const _size = 50.0;
+const _bgColor = Colors.blue;
+const _borderColor = Colors.black;
+const _borderWidth = 2.0;
+const _borderRadius = 25.0;
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Digit Pixel UI',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'IPZ-33: Vladyslav\'s First Flutter App'),
+      home: const MyHomePage(title: 'Digit Pixel UI'),
     );
   }
 }
@@ -30,14 +36,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter += 4;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,25 +43,86 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: _buildFour(),
+      ),
+    );
+  }
+
+  List<Widget> _buildFour() {
+    return [
+      _buildLineAround(),
+      _buildLineAround(),
+      _buildLineTop(),
+      _buildLineRight(),
+      _buildLineRight(),
+    ];
+  }
+
+  Widget _buildLineTop() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildDot(),
+        _buildDot(),
+        _buildDot(),
+      ],
+    );
+  }
+
+  Widget _buildLineLeft() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildDot(),
+        _buildEmpty(),
+        _buildEmpty(),
+      ],
+    );
+  }
+
+  Widget _buildLineRight() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildEmpty(),
+        _buildEmpty(),
+        _buildDot(),
+      ],
+    );
+  }
+
+  Widget _buildLineAround() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildDot(),
+        _buildEmpty(),
+        _buildDot(),
+      ],
+    );
+  }
+
+  Widget _buildDot() {
+    return Container(
+      width: _size,
+      height: _size,
+      decoration: BoxDecoration(
+        color: _bgColor,
+        borderRadius: const BorderRadius.all(Radius.circular(_borderRadius)),
+        border: Border.all(
+          color: _borderColor,
+          width: _borderWidth,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.pets),
-      ),
+    );
+  }
+
+  Widget _buildEmpty() {
+    return const SizedBox(
+      width: _size,
+      height: _size,
     );
   }
 }
